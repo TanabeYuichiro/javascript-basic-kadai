cd9f07ed12fa950bd55e917c1d95342a32a98925
TanabeYuichiro <79128764+TanabeYuichiro@users.noreply.github.com>
2025-02-03 21:00:15 +0900

    Update calc.js

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -5,12 +5,12 @@ let num = 1;
 // 変数numが3と5の倍数の場合： “3と5の倍数です”
 // それ以外の場合： 変数numの値を出力する
 
-if (num % 3 == 0) {
-    console.log("3の倍数です");
-} else if (num % 5 == 0) {
-    console.log("5の倍数です");
-} else if (num % 3 == 0 && num % 5 == 0) {
+if (num % 3 === 0 && num % 5 === 0) {
     console.log("3と5の倍数です");
+} else if (num % 3 === 0) {
+    console.log("3の倍数です");
+} else if (num % 5 === 0) {
+    console.log("5の倍数です");
 } else {
     console.log(num);
 }
