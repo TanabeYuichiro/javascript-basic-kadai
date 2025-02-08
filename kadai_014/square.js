df890cd3ce315981a4a0dcb80e4aa39182824dd0
TanabeYuichiro <79128764+TanabeYuichiro@users.noreply.github.com>
2025-02-08 18:25:30 +0900

    Add files via upload

---
+++ kadai_014/square.js
@@ -0,0 +1,11 @@
+// 1. 引数で渡した値を二乗する関数
+function square(number) {
+    return number * number;
+}
+
+// 引数の例（この値は自由に変更可能です）
+const input = 10;
+
+// 2. 関数を呼び出し、結果をコンソールに出力
+const result = square(input);
+console.log(`${input}の二乗は${result}です。`);
